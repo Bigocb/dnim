8c3aeac326752dd700915bab6540bf39daeef47f
Bobby Cloutier <bobby.cloutier@gmail.com>
2024-12-02 23:28:13 -0500

    small cleanup

--- src/app/frontend/frontend.py
+++ src/app/frontend/frontend.py
@@ -15,15 +15,18 @@ class Helpers:
     def __init__(self):
         pass
 
+
     def handle_click(self, arg):
             db.insert_topic(arg)
             self.get_topic.refresh
+
 
     def get_diff(self, old, current):
         old_doc = db.get_page_version(old['topic'], old['_id'])
         current_doc = db.get_page_version(current['topic'], current['_id'])
         diff = self.markdown_diff(old_doc['body'],current_doc['body'])
         return diff
+
 
     def markdown_diff(self, old_doc, current_doc):
         current = markdown.markdown(current_doc)
@@ -32,6 +35,7 @@ class Helpers:
         html = differ.make_file(old.splitlines(),current.splitlines())
         return html
     
+
     def get_headers(self):
         with ui.header(elevated=True).style('background-color: #3874c8').classes('items-center justify-between'):
             ui.button(on_click=lambda: left_drawer.toggle(), icon='menu').props('flat color=white')
@@ -40,13 +44,55 @@ class Helpers:
             ui.link('Topics','/topics')
             ui.link('New Topic','/new')
     
+
     def get_footers(self):
         with ui.footer().style('background-color: #3874c8'):
             ui.label('Created by TheJoeDev').style('color: white; font-size: 75%; font-weight: 300')
 
 
+    def get_topic(self, topic: str):
+
+        res = db.get_page(topic)
+        
+        new_page = Page(
+                topic=res['topic'],
+                body=None,
+                ts = datetime.now(),
+                tags=res['tags']
+            )
+        with ui.card().classes('col-span-4 md:col-span-4 row-span-1 h-full'):
+                ui.separator()
+                with ui.row():
+                    ui.label(f"{res['topic']}").style('color: #6E93D6; font-size: 200%; font-weight: 300')
+                    switch = ui.switch()
+                    ui.chip(icon='save', on_click=lambda: db.insert_topic(new_page)).bind_visibility_from(switch, 'value')
+                ui.separator()
+                p = ui.codemirror(f"{res['body']}",language='Python').classes('h-32').bind_value_to(
+                target_object=new_page, target_name="body").classes('h-full').bind_visibility_from(switch, 'value')
+                # a ui markdown component that is visible with the switch variable is off
+                ui.markdown()
+                ui.markdown().bind_content_from(p, 'value',
+                                backward=lambda v: f'{v}')
+                ui.separator()
+                resp = db.get_versions(res['topic'])
+
+        with ui.card().classes('col-span-4 md:col-span-4 row-span-1 h-full') as preview:
+            ui.label(f"Versions").style('color: #6E93D6; font-size: 200%; font-weight: 300')
+            ui.separator()
+            for i in resp:
+                with ui.card().classes('col-span-1 md:col-span-1 row-span-1 h-full w-full'):
+                    with ui.dialog() as dialog, ui.card().classes('w-full h-full').style('max-width: none'):
+                        ui.label(f"{i['ts']} vs. Current")
+                        ui.html(self.get_diff(i, res))
+                        ui.button('Close', on_click=dialog.close)
+                    ui.button(f"{i['ts']}", on_click=dialog.open)
+
+
 def init(fastapi_app: FastAPI) -> None:
+
+    # Bring in the helper functions
     h = Helpers()
+
 
     @ui.page('/')
     def show():
@@ -83,11 +129,6 @@ def init(fastapi_app: FastAPI) -> None:
         ui.checkbox('dark mode').bind_value(app.storage.user, 'dark_mode')
         h.get_footers()
 
-    ui.run_with(
-        fastapi_app,
-        mount_path='/gui',  # NOTE this can be omitted if you want the paths passed to @ui.page to be at the root
-        storage_secret='pick your private secret here',  # NOTE setting a secret is optional but allows for persistent storage per user
-    )
 
     @ui.page('/topics')
     def show():
@@ -107,63 +148,23 @@ def init(fastapi_app: FastAPI) -> None:
                 storage_secret='pick your private secret here',  # NOTE setting a secret is optional but allows for persistent storage per user
             )
     
-    
-    @ui.refreshable
-    def get_topic(topic: str):
-
-        res = db.get_page(topic)
-        
-        new_page = Page(
-                topic=res['topic'],
-                body=None,
-                ts = datetime.now(),
-                tags=res['tags']
-            )
-        with ui.card().classes('col-span-4 md:col-span-4 row-span-1 h-full'):
-                ui.separator()
-                with ui.row():
-                    ui.label(f"{res['topic']}").style('color: #6E93D6; font-size: 200%; font-weight: 300')
-                    switch = ui.switch()
-                    ui.button('refresh', on_click=lambda: get_topic.refresh)
-                    ui.chip(icon='save', on_click=lambda: db.insert_topic(new_page)).bind_visibility_from(switch, 'value')
-                ui.separator()
-                p = ui.codemirror(f"{res['body']}",language='Python').classes('h-32').bind_value_to(
-                target_object=new_page, target_name="body").classes('h-full').bind_visibility_from(switch, 'value')
-                # a ui markdown component that is visible with the switch variable is off
-                ui.markdown()
-                ui.markdown().bind_content_from(p, 'value',
-                                backward=lambda v: f'{v}')
-                ui.separator()
-                resp = db.get_versions(res['topic'])
-
-        with ui.card().classes('col-span-4 md:col-span-4 row-span-1 h-full') as preview:
-            ui.label(f"Versions").style('color: #6E93D6; font-size: 200%; font-weight: 300')
-            ui.separator()
-            for i in resp:
-                with ui.card().classes('col-span-1 md:col-span-1 row-span-1 h-full w-full'):
-                    with ui.dialog() as dialog, ui.card().classes('w-full h-full').style('max-width: none'):
-                        ui.label(f"{i['ts']} vs. Current")
-                        ui.html(h.get_diff(i, res))
-                        ui.button('Close', on_click=dialog.close)
-                    ui.button(f"{i['ts']}", on_click=dialog.open)
 
     @ui.page('/topics/{topic}')
     def show_page_single(topic:str):
 
         h.get_headers()
         
-        
         with ui.grid(columns=4).classes('gap-4 w-full h-screen p-4'):
-            get_topic(topic)
+            h.get_topic(topic)
             
-                        
         h.get_footers()
+
 
     @ui.page('/new')
     def show():
         h.get_headers()
-        ui.menu()
-        ui.separator()
+
+        # create a new page object
         page = Page(
                 topic=None,
                 body=None,
@@ -190,10 +191,9 @@ def init(fastapi_app: FastAPI) -> None:
                                 backward=lambda v: f'{v}')
 
         h.get_footers()
-        ui.run_with(
-            fastapi_app,
-                mount_path='/topics',  # NOTE this can be omitted if you want the paths passed to @ui.page to be at the root
-                storage_secret='pick your private secret here',  # NOTE setting a secret is optional but allows for persistent storage per user
-            )
 
-
+    ui.run_with(
+        fastapi_app,
+        mount_path='/gui',  # NOTE this can be omitted if you want the paths passed to @ui.page to be at the root
+        storage_secret='pick your private secret here',  # NOTE setting a secret is optional but allows for persistent storage per user
+    )
